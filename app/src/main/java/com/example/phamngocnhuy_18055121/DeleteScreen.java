c8de930c1a021ff3b3464d34ffd86813a320d993
YtalyPham <nhuyphamdev@gmail.com>
2021-06-02 19:24:20 +0700

    xong ShowStudentListAPI va btnButton + hinh ShowStudentListScr

---
+++ app/src/main/java/com/example/phamngocnhuy_18055121/DeleteScreen.java
@@ -0,0 +1,14 @@
+package com.example.phamngocnhuy_18055121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_screen);
+    }
+}
